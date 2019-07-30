33a80d7f87be70b92b4104b46e514c93076b787e
wangbaojie <wbj@ziziv.com>
2019-07-30 18:30:36 +0800

    退出登录后跳转登录页面

--- src/modules/auth/index.js
+++ src/modules/auth/index.js
@@ -1,10 +1,2 @@
-const routes = [
-    {
-        path: 'auth/login',
-        name: 'auth.login',
-        component: () => import('./login')
-    }
-]
-
-export { routes }
+export { default as routes } from './routes'
 export { default as vuex } from './vuex'
