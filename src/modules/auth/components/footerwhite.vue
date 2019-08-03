2c640b3fc368c5bccbae6ae5cf2f45434719520f
wangbaojie <wbj@ziziv.com>
2019-08-03 15:05:41 +0800

    修改登录页面底部

--- src/modules/auth/components/footerwhite.vue
+++ src/modules/auth/components/footerwhite.vue
@@ -1,72 +1,76 @@
 <template>
-  <div class="footer">
-    <div class="nav">
-      <a href="javascript:;">关于我们</a>
-      <span>|</span>
-      <a href="javascript:;">联系我们</a>
-      <span>|</span>
-      <a href="javascript:;">隐私保护</a>
-      <span>|</span>
-      <a href="javascript:;">免责声明</a>
-      <span>|</span>
-      <a href="javascript:;">诚聘英才</a>
-    </div>
-    <p class="license">【网络文化经营许可证】：陕网文许字[xxxx]xxxx-xxx号|【增值电信业务经营许可证】：陕xx-xxxxxxxx【工信部</p>
-    <p class="copyright">西安学学信息技术有限公司 © xxxx-xxxx soogua.cn 版权所有</p>
-    <p class="address">公司地址：陕西省西安市雁塔区高新三路蓝溪国际酒店 1号写字楼18F 联系电话：400-000-0000</p>
-  </div>
+	<div class="footer">
+		<div class="nav">
+			<router-link :to="{name:'home'}">首页</router-link>
+			<span>|</span>
+			<router-link :to="{name:'gamecenter'}">游戏</router-link>
+			<span>|</span>
+			<router-link :to="{name:'goods.flow'}">流量</router-link>
+			<span>|</span>
+			<router-link :to="{name:'goods.account'}">账号</router-link>
+			<span>|</span>
+			<router-link :to="{name:'goods.service'}">服务</router-link>
+			<span>|</span>
+			<router-link :to="{name:'person.person'}">个人中心</router-link>
+		</div>
+		<p class="copyright"><img src="~$assets/images/logo_copy.png"
+					 alt>搜瓜提供店铺服务</p>
+	</div>
 </template>
 
 <script>
-export default {};
+	export default {};
 </script>
 
 <style lang="scss" scoped>
-.footer {
-  font-size: 12px;
-  font-weight: 500;
-  line-height: 13px;
-  background-color: #fff;
-  padding-top: 40px;
-  padding-bottom: 30px;
-  .nav {
-    font-size: 12px;
-    font-weight: 500;
-    line-height: 13px;
-    text-align: center;
-    a {
-      color: #999;
-    }
-    span {
-      color: #999;
-      margin-left: 10px;
-      margin-right: 10px;
-    }
-  }
-}
-.license {
-  font-size: 12px;
-  font-weight: 500;
-  line-height: 13px;
-  text-align: center;
-  color: #999;
-  margin-top: 10px;
-}
-.copyright {
-  font-size: 12px;
-  font-weight: 500;
-  line-height: 13px;
-  text-align: center;
-  color: #999;
-  margin-top: 10px;
-}
-.address {
-  font-size: 12px;
-  font-weight: 500;
-  line-height: 13px;
-  text-align: center;
-  color: #999;
-  margin-top: 10px;
-}
-
+	.footer {
+		font-size: 12px;
+		font-weight: 500;
+		line-height: 13px;
+		background-color: #fff;
+		padding-top: 40px;
+		padding-bottom: 30px;
+		.nav {
+			font-size: 12px;
+			font-weight: 500;
+			line-height: 13px;
+			text-align: center;
+			a {
+				color: #999;
+			}
+			span {
+				color: #999;
+				margin-left: 10px;
+				margin-right: 10px;
+			}
+		}
+	}
+	.license {
+		font-size: 12px;
+		font-weight: 500;
+		line-height: 13px;
+		text-align: center;
+		color: #999;
+		margin-top: 10px;
+	}
+	.copyright {
+		font-size: 12px;
+		font-weight: 500;
+		line-height: 13px;
+		text-align: center;
+		color: #999;
+		margin-top: 10px;
+		img {
+			height: 10px;
+			padding-right: 10px;
+		}
+	}
+	.address {
+		font-size: 12px;
+		font-weight: 500;
+		line-height: 13px;
+		text-align: center;
+		color: #999;
+		margin-top: 10px;
+	}
 </style>
